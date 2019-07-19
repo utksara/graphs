cd743c34bdabcd1ce361b56b270a7ee68c6e2112
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2019-07-19 13:46:35 +0530

    Add files via upload

---
+++ airport_sim_2/airport_sim_2/ConsoleApplication1/graph.h
@@ -0,0 +1,34 @@
+#pragma once
+class node {
+	int index;
+	bool state;
+	node* next1 = NULL;
+	node* next2 = NULL;
+	int vacancy;
+
+public:
+	const float duration;
+
+	node();
+	node(int index, float Duration, int vacancy);
+
+	void show_index();
+	void show_network();
+
+	bool isvacant();
+	bool isterminal();
+
+	void link(node* n1, node* n2);
+	void link(node* n1);
+
+	node* goto_next_node();
+
+	void change_vacancy(int N);
+
+	void update_state();
+	void occupy();
+	void reset();
+};
+
+
+
---
+++ airport_sim_2/airport_sim_2/ConsoleApplication1/network.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include<iostream>
+#include "graph.h"
+
+network::network(int arr[]) {
+
+}
